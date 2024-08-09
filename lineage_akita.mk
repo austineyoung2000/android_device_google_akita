0dc4b1130d090a8985293950043178814828b6a6
Austin Young <austineyoung2000@gmail.com>
2024-08-08 22:10:17 -0400

    akita: Import some flags for risingOS

--- lineage_akita.mk
+++ lineage_akita.mk
@@ -19,6 +19,12 @@ PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 8a
 PRODUCT_NAME := lineage_akita
 
+# Addons
+TARGET_HAS_UDFPS := true
+
+# Gms 
+WITH_GMS := true
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
