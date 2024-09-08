425657e8abe3ed24a2d2a7ab0111374db9e16bb3
Austin Young <austineyoung2000@gmail.com>
2024-09-08 13:21:17 -0400

    akita: BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES
    Change-Id: I32ea1910d8de6521f6b284a29e150bbe8d112f5e

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -6,6 +6,7 @@
 
 DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += device/google/akita/aam_compatibility_matrix.xml
 BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 DEVICE_PATH := device/google/akita
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_akita
