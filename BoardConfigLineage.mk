ee1418eb5bbb1eb97999355723e3aa0b10a0e037
Austin Young <austineyoung2000@gmail.com>
2024-10-28 17:00:41 -0400

    akita: RisingOS Upkeep
    Change-Id: I40a0399df87c54b188ee6f39b2cbbaba1ffb7315

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -4,7 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/aam_compatibility_matrix.xml
+DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += device/google/akita/aam_compatibility_matrix.xml
+BUILD_BROKEN_DUP_RULES := true
 DEVICE_PATH := device/google/akita
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_akita
